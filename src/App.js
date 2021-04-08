8277c0b06a6b0f8d9c3f8af2de8276d5216ac9be
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-04-08 14:28:40 +0100

    Using State

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 
 import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+
+  const [todos, setTodos] = useState(['Help', 'B', 'C', 'D', 'E']);
+
   return (
     <div className="App">
       <h1> HELLO WORLD </h1>
+      <input onChange={setTodos} />
+      <button> Add a Task </button>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}> {todo} </li>
+        ))}
+        <li></li>
+
+
+      </ul>
     </div>
   );
 }
